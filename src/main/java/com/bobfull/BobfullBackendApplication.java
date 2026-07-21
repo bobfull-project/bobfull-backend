3c6334edfe24f9098241152084e35e47e8567a4f
HS <ehehqp@gmail.com>
2026-07-22 08:54:52 +0900

    초기 Spring Boot 및 Docker MySQL 개발 환경 구성

---
+++ src/main/java/com/bobfull/BobfullBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bobfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobfullBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobfullBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobfull/BobfullBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobfullBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
